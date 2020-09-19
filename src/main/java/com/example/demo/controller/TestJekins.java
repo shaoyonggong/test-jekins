09784274be883d34467d9fc8fe6c5a7fc262e524
shaoyonggong <shaoyonggong@126.com>
2020-09-19 17:59:54 +0800

    1

---
+++ src/main/java/com/example/demo/controller/TestJekins.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @Description
+ * @Author shaoyonggong
+ * @Date 2020/9/19
+ */
+@Controller
+@RequestMapping(value = "/")
+public class TestJekins {
+    @ResponseBody
+    @RequestMapping(value = "/data")
+    public String data() {
+        return "hello";
+    }
+}
